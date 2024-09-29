0e6cdc7c66b9524f34ea6477ad0f080866e2d14d
Bruno Oliveira <5279392+brunofunnie@users.noreply.github.com>
2024-09-29 09:57:24 -0300

    fix: Fix build for Jenkins

--- docker/confs/entrypoint.sh
+++ docker/confs/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp /app/.env /src/.env
-/usr/local/bin/php /src/laracord app:build --build-version=unreleased
-mv /src/builds/laracord /app/laracord
-/app/laracord migrate
+cd /src && /usr/local/bin/php laracord app:build --build-version=unreleased && mv builds/laracord /app/laracord
+cd /app && /usr/local/bin/php /app/laracord migrate
+
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
